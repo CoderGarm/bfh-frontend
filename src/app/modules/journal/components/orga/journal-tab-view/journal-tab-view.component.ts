07a254764454a946ecc1a66afbbba68972bba5de
CoderGarm <kar.lie@gmx.de>
2023-01-27 12:04:33 +0100

    add new battle reports marker

--- src/app/modules/journal/components/orga/journal-tab-view/journal-tab-view.component.ts
+++ src/app/modules/journal/components/orga/journal-tab-view/journal-tab-view.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {SubscriptionManager} from "../../../../../SubscriptionManager";
 import {TypeService} from "../../../../../services/type.service";
 import {BackgroundService} from "../../../../../services/background.service";
+import {BattleReportApiService} from "../../../../../services/swagger";
 
 @Component({
     selector: 'app-journal-tab-view',
@@ -12,16 +13,22 @@ export class JournalTabViewComponent extends SubscriptionManager implements OnIn
 
     static path: string = 'journal';
 
+    hasNewReports: boolean = false;
+
     /**
      * @param typeService needed to fetch types after login
+     * @param battleReportService nothing special
      * @param backgroundService needed after run long-running queries after login
      */
     constructor(private typeService: TypeService,
+                private battleReportService: BattleReportApiService,
                 private backgroundService: BackgroundService) {
         super();
     }
 
     ngOnInit(): void {
+        let sub = this.battleReportService.hasNewReportsForUser().subscribe(resp => this.hasNewReports = resp);
+        this.subscriptions.push(sub);
     }
 
 }
