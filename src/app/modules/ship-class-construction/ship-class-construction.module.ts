b3a1f955e7df0693e8de96f0080e5f0ea38b51d2
CoderGarm <kar.lie@gmx.de>
2023-05-10 14:11:02 +0200

    clickable cursor in forum

--- src/app/modules/ship-class-construction/ship-class-construction.module.ts
+++ src/app/modules/ship-class-construction/ship-class-construction.module.ts
@@ -26,7 +26,8 @@ import {ShipClassFittingModifyComponent} from "./components/payload/ship-class-f
         FittingCreateComponent,
     ],
     exports: [
-        ShipClassSelectionComponent
+        ShipClassSelectionComponent,
+        ShipClassNamePatternValidatorDirective
     ],
     imports: [
         SharedModuleModule,
