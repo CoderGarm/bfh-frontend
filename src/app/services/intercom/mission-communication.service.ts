e36d005c8258d282c45be79f1f603946c0a4008b
CoderGarm <kar.lie@gmx.de>
2023-07-12 15:42:54 +0200

    adding missions administration

--- src/app/services/intercom/mission-communication.service.ts
+++ src/app/services/intercom/mission-communication.service.ts
@@ -1,11 +1,16 @@
 import {Injectable} from "@angular/core";
-import {FleetApiService, Mission, MissionApiService} from "../swagger";
+import {FleetApiService, Mission, MissionApiService, WarShip} from "../swagger";
 import {SubscriptionManager} from "../../subscription.manager";
+import MissionTypeEnum = Mission.MissionTypeEnum;
 
 @Injectable()
 export class MissionCommunicationService extends SubscriptionManager {
 
     activeMissions: Mission[] = [];
+
+    pooledShips: WarShip[] = [];
+
+    playerMissions: MissionTypeEnum[] = [MissionTypeEnum.PIRATE_HUNT, MissionTypeEnum.CONVOY_PROTECTION];
 
     constructor(private missionService: MissionApiService,
                 private fleetService: FleetApiService) {
@@ -14,6 +19,13 @@ export class MissionCommunicationService extends SubscriptionManager {
 
     fetchMissions() {
         let sub = this.missionService.getMissions().subscribe(resp => this.activeMissions = resp);
+        this.subscriptions.push(sub);
+    }
+
+    fetchPooledShips() {
+        let sub = this.fleetService.getPooledWarships().subscribe(resp =>
+            this.pooledShips = resp
+        );
         this.subscriptions.push(sub);
     }
 }
