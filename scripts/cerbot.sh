aac271e4cdb51833bdfddff83863b5b7416f43e4
CoderGarm <kar.lie@gmx.de>
2023-08-26 16:52:39 +0200

    add media certificate

--- scripts/cerbot.sh
+++ scripts/cerbot.sh
@@ -2,7 +2,7 @@
 
 echo "Stopping apache"
 sudo systemctl stop apache2.service;
-if sudo certbot certonly --standalone -d battleforhonor.de -d www.battleforhonor.de -d map.battleforhonor.de; then
+if sudo certbot certonly --standalone -d battleforhonor.de -d www.battleforhonor.de -d map.battleforhonor.de -d media.battleforhonor.de; then
   echo "certificates updated - please check all sites enabled"
   sudo systemctl start apache2.service;
   echo "Apache restarted"
