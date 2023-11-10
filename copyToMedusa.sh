c6a7aafbb3f15b65110cd18405a1d4a36cc749eb
CoderGarm <kar.lie@gmx.de>
2023-11-10 14:15:10 +0100

    add upload script warning nd fixme

--- copyToMedusa.sh
+++ copyToMedusa.sh
@@ -2,8 +2,15 @@
 
 # run it on build system
 
+# fixme schnellstens
+echo 'bitte lade es manuell hoch, der Prozess funktioniert momentan nicht'
+echo 'cp scripts/htaccess dist/bfh-fe/.htaccess'
+echo 'scp -r dist/ medusa:uploadTarget/'
+echo 'Und dann natürlich deployen'
+exit 1
+
 scp scripts/deployFrontend.sh medusa:
 cp scripts/google2cbaff394ce44864.html dist/bfh-fe/
 cp scripts/htaccess dist/bfh-fe/.htaccess
-rsync -a --ignore-existing --delete --progress dist/ medusa:/home/karsten/uploadTarget/bhf-frontend/
-rm -rf dist/
+rsync -a --ignore-existing --delete --progress dist/ medusa:/home/karsten/uploadTarget/bfh-frontend/
+#rm -rf dist/
