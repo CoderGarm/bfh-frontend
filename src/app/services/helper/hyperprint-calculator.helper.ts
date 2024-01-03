49c78965a66797c4fb49ed1c1daf2225209e7a9f
CoderGarm <kar.lie@gmx.de>
2024-01-03 11:21:42 +0100

    fix unreliable eloka sensor values

--- src/app/services/helper/hyperprint-calculator.helper.ts
+++ src/app/services/helper/hyperprint-calculator.helper.ts
@@ -17,19 +17,11 @@ export class HyperprintCalculatorHelper {
     static getResolvedTonnage(tonnage: Mass, hyperPrintSensorValue: number): MassRange {
         const kiloTons = Math.floor(NavigationCalculator.convertMassToMetric(tonnage, MassMetricEnum.KT));
         const sensorAccuracy = Math.floor(hyperPrintSensorValue - kiloTons);
-        const deviationFactor = Math.abs(hyperPrintSensorValue / sensorAccuracy);
+        const deviationFactor = Number.parseFloat('0.' + Math.abs(sensorAccuracy));
         let absoluteDeviation = HyperprintCalculatorHelper.randomIntFromInterval(0, kiloTons * deviationFactor);
         if (sensorAccuracy >= 0) {
             absoluteDeviation = HyperprintCalculatorHelper.randomIntFromInterval(0, kiloTons * 0.1);
         }
-
-        console.log(
-            "hyperPrintSensorValue", hyperPrintSensorValue,
-            "kiloTons", kiloTons,
-            "sensorAccuracy", sensorAccuracy,
-            "deviationFactor", deviationFactor,
-            "absoluteDeviation", absoluteDeviation
-        )
         return {
             low: {coordinate: kiloTons - absoluteDeviation, massMetric: MassMetricEnum.KT},
             high: {coordinate: kiloTons + absoluteDeviation, massMetric: MassMetricEnum.KT}
